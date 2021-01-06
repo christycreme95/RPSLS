58400edbdd5cce6598bf6aecef782197c5928e62
Christy Eang <christyeang@yahoo.com>
2021-01-06 10:33:32 -0800

    that's so fetch

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,20 @@
 
 
 
-function loadCPU(url) {
-    fetch(url).then(data => {
-        data.text().then(data => {
-            console.log(data)
-            
-        })
-    })
+
+// fetch("https://csa2020studentapi.azurewebsites.net/rpsls")
+//     .then(data => {
+//     data.text()
+//     .then(value=>console.log(value))
+// })
+
+let info;
+async function getCPU(){
+    let promise = await fetch("https://csa2020studentapi.azurewebsites.net/rpsls")
+    info = await promise.text();
+    console.log(info);
 }
-loadCPU("https://csa2020studentapi.azurewebsites.net/rpsls")
+getCPU();
+
 
 
