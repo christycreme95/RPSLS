5aba2cf4cf210cfbd4321bd5b57e2dbccd042374
Christy Eang <christyeang@yahoo.com>
2021-01-06 09:59:26 -0800

    api set up to return something

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-// Christy Eang
+
+
+
+
+
+
+
+function loadCPU(url) {
+    fetch(url).then(data => {
+        data.text().then(data => {
+            console.log(data)
+            
+        })
+    })
+}
+loadCPU("https://csa2020studentapi.azurewebsites.net/rpsls")
+
+
