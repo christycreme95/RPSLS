26f1b0ed43de124bcad86e6b400d35c654b98fa7
Christy Eang <christyeang@yahoo.com>
2021-01-06 13:20:16 -0800

    get elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+let cpuAns;
+let vComputer = document.getElementById("vComputer");
+let twoPlayer = document.getElementById("twoPlayer");
+let vsJT = document.getElementById("vsJT");
+let game;
 
-
+vComputer.addEventListener('click', function(){
+    // open page or inject or whatever into this and everything goes here.
+    
+})
 
 
 
@@ -15,8 +23,8 @@
 let info;
 async function getCPU(){
     let promise = await fetch("https://csa2020studentapi.azurewebsites.net/rpsls")
-    info = await promise.text();
-    console.log(info);
+    cpuAns = await promise.text();
+    console.log(cpuAns)
 }
 getCPU();
 
