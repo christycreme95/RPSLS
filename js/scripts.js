26131828c2e0d90f00ae00f11ae332226ee55e40
Christy Eang <christyeang@yahoo.com>
2021-01-06 17:35:34 -0800

    made my icons buttons

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,43 @@ let game = false;
 let info;
 let injectHere = document.getElementById("injectHere");
 
+
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let lizard = document.getElementById("lizard");
+let spock = document.getElementById("spock");
+
+
+
+
+
+
+
+
+
 vComputer.addEventListener('click', function(){
  deleteEverything();
-setTimeout(injectChoice("../pages/choice.html"),1000)
-})
-console.log(game)
+setTimeout(inject("../pages/choice.html"),1000)
 
+})
+
+twoPlayer.addEventListener('click',()=>{
+    deleteEverything()
+    setTimeout(inject("../pages/choice.html"),1000)
+})
 
 async function getCPU(){
     let promise = await fetch("https://csa2020studentapi.azurewebsites.net/rpsls")
     cpuAns = await promise.text();
-    console.log(cpuAns)
 }
-getCPU();
+
 
 function deleteEverything(){
     injectHere.innerHTML="";
 }
 
-function injectChoice(url){
+function inject(url){
     fetch(url).then(
         data=>{
             // console.log(data)
@@ -35,4 +53,11 @@ function injectChoice(url){
             )
         }
     )
+}
+
+function compGame(){
+rock.addEventListener('click', ()=>{
+    deleteEverything()
+    setTimeout(inject("../pages/finals.html"))
+})
 }
