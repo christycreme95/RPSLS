4ff500738d8e596910d2106a041fdf9876704720
Christy Eang <christyeang@yahoo.com>
2021-01-06 20:52:18 -0800

    compare function works, can't figure out how to find and take the buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
+let userAns="Rock";
 let cpuAns;
+let arr = [0 , 0, 0];
 let vComputer = document.getElementById("vComputer");
 let twoPlayer = document.getElementById("twoPlayer");
 let vsJT = document.getElementById("vsJT");
 let game = false;
 let info;
 let injectHere = document.getElementById("injectHere");
-
 
 let rock = document.getElementById("rock");
 let paper = document.getElementById("paper");
@@ -15,15 +16,10 @@ let spock = document.getElementById("spock");
 
 
 
-
-
-
-
-
-
 vComputer.addEventListener('click', function(){
- deleteEverything();
-setTimeout(inject("../pages/choice.html"),1000)
+    deleteEverything();
+    getCPU();
+    setTimeout(inject("../pages/choice.html"),500)
 
 })
 
@@ -35,6 +31,7 @@ twoPlayer.addEventListener('click',()=>{
 async function getCPU(){
     let promise = await fetch("https://csa2020studentapi.azurewebsites.net/rpsls")
     cpuAns = await promise.text();
+    compare();
 }
 
 
@@ -56,8 +53,60 @@ function inject(url){
 }
 
 function compGame(){
-rock.addEventListener('click', ()=>{
-    deleteEverything()
-    setTimeout(inject("../pages/finals.html"))
-})
+    rock.addEventListener('click', ()=>{
+        deleteEverything()
+        setTimeout(inject("../pages/finals.html"))
+        userAns="Rock";
+
+    })
+}
+
+function compare(){
+    if(cpuAns==userAns){
+        console.log("Tie")
+    }else if(userAns=="Rock"){
+        if(cpuAns=="Paper" || cpuAns == "Spock"){
+            // user loses
+            console.log("You Lose")
+        }else if(cpuAns=="Scissors" || cpuAns == "Lizard"){
+            // user wins
+            console.log("You Win")
+            
+        }
+    }else if(userAns=="Paper"){
+        if(cpuAns=="Scissors" || "Lizard"){
+            // user loses
+            console.log("You Lose")
+        }else if(cpuAns=="Rock" || "Spock"){
+            // user wins
+            console.log("You Win")
+        }
+    }else if(userAns=="Scissors"){
+        if(cpuAns=="Spock" || "Rock"){
+            // user loses
+            console.log("You Lose")
+        }else if(cpuAns=="Scissors" || "Paper"){
+            // user wins
+            console.log("You Win")
+        }
+    }
+    else if(userAns=="Lizard"){
+        if(cpuAns=="Rock" || "Scissors"){
+            // user loses
+            console.log("You Lose")
+        }else if(cpuAns=="Paper" || "Spock"){
+            // user wins
+            console.log("You Win")
+        }
+    }else if(userAns=="Spock"){
+        if(cpuAns=="Paper" || "Lizard"){
+            // user loses
+            console.log("You Lose")
+        }else if(cpuAns=="Rock" || "Scissors"){
+            // user wins
+            console.log("You Win")
+        }
+    }else{
+        console.log("We have an issue")
+    }
 }
